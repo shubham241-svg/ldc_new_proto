222dddd530ccf5972fc188bd5970f3e70a48b0c2
Shubham Gupta <justliving251@gmail.com>
2026-03-03 23:51:08 +0530

    Add files via upload

---
+++ src/data/ldcdata.ts
@@ -0,0 +1,35 @@
+// LDC ID PATCH API:
+ 
+// /{match_id}
+// Payload:
+// {
+//   "ldc_id": "0008042534",
+//   "batch_id": "5de8c41e-40df-4547-9936-4bb534b7236c"
+// }
+
+export const ldcapidata = 
+
+ {
+  "id": 7439,
+  "batch_id": "5de8c41e-40df-4547-9936-4bb534b7236c",
+  "bill_of_lading": "134362/11230",
+  "load_location": "UNKNOWN LOCATION",
+  "license_plate": "CY357178",
+  "load_weight": 34.611,
+  "load_date": null,
+  "unload_date": "2026-02-04",
+  "unload_weight": 34.611,
+  "customer_scale_ticket_number": "5028461457.0",
+  "ldc_id": "0008042534",
+  "ldc_bol": "29/LAP00025199",
+  "ldc_plant": "MALMESBURY WC KAA ZA 1122",
+  "ldc_vehicle": "HS02HGGP",
+  "ldc_weight": 35.58,
+  "ldc_date": "2026-02-05",
+  "overridden_ldc_id": "0008042534",
+  "match_context": "BOL+Mass (veh 100.0%; Received Mass within 0.26%; Date within 0 days; Location 33.2% similar)",
+  "status": "MATCHED",
+  "is_validated": true,
+  "generation_id": null,
+  "created_at": "2026-03-03T15:18:23.912023"
+}
